22963d50b8e445118c1f1359030c0b194fb16dac
deepesh728 <deepeshojhaee@gmail.com>
2018-08-07 04:42:06 -0700

    added sets.py

---
+++ sets.py
@@ -0,0 +1,8 @@
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+fruit = set(basket)
+print(fruit)
+set(['orange', 'pear', 'apple', 'banana'])
+'orange' in fruit
+True
+'crabgrass' in fruit
+False
